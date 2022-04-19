b86ef784cbdf9653188b6efc6e5295665f76f50f
ABCorabc <kangkang1104@naver.com>
2022-04-19 13:37:35 +0900

    클래스 연습 파일 복사

--- Person.py
+++ Person.py
@@ -1,16 +1,13 @@
-# class1.py
-#클래스 정의
+# Person.py 
 class Person:
-    #초기화 메서드(생성자)
     def __init__(self):
         self.name = "default name"
-    #인스턴스 메서드
     def print(self):
         print("My name is {0}".format(self.name))
 
-#인스턴스 생성
+
 p1 = Person()
-p2 = Person()     
+p2 = Person()
 p1.name = "전우치"
 p1.print()
 p2.print()
